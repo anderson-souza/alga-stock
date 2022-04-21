457534f7ef66d352dcb0d4b8e505975fe72ec0ed
Anderson Souza <andersonpds14@gmail.com>
2022-04-21 11:21:24 -0300

    Add header; Add Button;

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import Button from "../Button";
+import Header from "../Header";
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Header title="AlgaStock" />
+      <div className="Container">
+        <Button
+          content="Teste"
+          onClick={() => alert("Bora")}
+          appendIcon={<h1>Bora</h1>}
+        >
+          Macarroni
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from "react";
+import "./Button.css";
+
+declare interface ButtonProps {
+  content?: string;
+  onClick: () => void;
+  appendIcon?: ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+  return (
+    <button className="AppButton" onClick={props.onClick}>
+      {props.children || props.content}
+      {props.appendIcon}
+    </button>
+  );
+};
+
+export default Button;

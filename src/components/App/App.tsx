a827343125ce066782b4d497feeaaf05577998d1
Anderson Souza <andersonpds14@gmail.com>
2022-12-10 17:09:19 -0300

    feat: add login and profile view

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,23 @@
+// @ts-nocheck
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "../Header";
-import Container from "../../shared/Container";
-import ProductsCRUD from "../Products/ProductsCRUD";
+import HomeView from "../../views/HomeView";
+import NotFoundView from "../../views/NotFoundView";
+import LoginView from "../../views/LoginView";
+import ProfileView from "../../views/ProfileView";
 
 function App() {
   return (
     <div className="App">
-      <Header title="AlgaStock" />
-      <Container>
-        <ProductsCRUD />
-      </Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/login" element={<LoginView />} />
+          <Route path="/profile" element={<ProfileView />} />
+          <Route path="*" element={<NotFoundView />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

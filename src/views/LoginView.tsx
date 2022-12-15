0ca90cce325879ef8aca572eb4dce32caa35f1b8
Anderson Souza <andersonpds14@gmail.com>
2022-12-14 22:21:27 -0300

    style: improved app and login style

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LoginForm from "../components/Authentication/LoginForm";
+import background from "../static/background.jpeg";
 
 const LoginView = () => {
   return (
@@ -9,9 +10,10 @@ const LoginView = () => {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        backgroundImage: `url(${background})`,
       }}
     >
-      <div style={{ minWidth: 320 }}>
+      <div style={{ minWidth: 420 }}>
         <LoginForm />
       </div>
     </div>
